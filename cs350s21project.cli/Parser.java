d2197d589a01d491db5a0daca45c527c661b44e1
Nicholas Johnson <nicholasj@eagles.ewu.edu>
2021-05-21 15:53:48 -0700

    Parser Rough Draft

---
+++ cs350s21project.cli/Parser.java
@@ -0,0 +1,395 @@
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Parser {
+    // Custom Variables, not called for in specs
+    String command;
+    HashSet<String> keyWords = new HashSet<>();
+    String userInput;
+    String[] words;
+    String fileName;
+
+    // Universal Variables
+    String id;
+    String id2;
+    String id3;
+    String[] idN;
+
+    // Views Variables
+    int size;
+    String latitude; // TODO: datatype will be Latitude 45*30'15"
+    String longitude; // TODO: datatype will be Longitude 110*30'10.3"
+
+    // Actors variables
+    int coordinates;
+    int course; // TODO: datatype will be Course 090
+    int speed; // TODO: datatype will be Groundspeed 25
+    int altitude; // TODO: datatype will be Altitude 1000
+
+    // Munitions Variables
+    double time;// TODO: datatype will be Time 10.8
+    double distance; // TODO: datatype will be DistanceNauticalMiles 25.3
+    double azimuth; // TODO: datatype will be AttitudeYaw 10
+    double elevation; // TODO: datatype will be AttitudePitch 25
+
+    // Sensors/Fuzes Variables
+    double fov;// TODO: datatype will be FieldOfView 10
+    double power;// TODO: datatype will be Power 10
+    double sensitivity;// TODO: datatype will be Sensitivity 10
+    // altitude // already used in Actor
+    // distance
+    // time
+
+    public void runParser() {
+        getUserInput();
+        parseInput();
+        determineCommand();
+    }
+
+    public void getUserInput() {
+        Scanner scanner = new Scanner(new InputStreamReader(System.in));
+        this.userInput = scanner.nextLine(); // TODO: remove comment after testing and remove below line
+
+    }
+
+    public void parseInput() {
+        this.words = this.userInput.split(" ");
+    }
+
+    public void determineCommand() {
+        views();
+        actors();
+        munitions();
+        sensorsFuzes();
+        miscellaneous();
+    }
+
+    public void views() {
+        // I. VIEWS
+        if (this.words[1].equals("window")) {
+            switch(this.words[0]) {
+                case "create":
+                    // create window id top view with size (latitude1 latitude2 latitude3) (longitude longitude2 longitude3)
+                    // TODO: NICK - parse out variables // needs clarification from Luis first
+                    System.out.println("Use CommandViewCreateWindowTop");
+                    // TODO: Use CommandViewCreateWindowTop
+                    break;
+                case "delete":
+                    // delete window id
+                    // TODO: NICK - parse out variables // needs clarification from Luis first
+                    System.out.println("Use CommandViewDeleteWindow");
+                    // TODO: Use CommandViewDeleteWindow
+                    break;
+            }
+        }
+    }
+
+    public void actors() {
+        // II. ACTORS
+        if (this.words[1].equals("ship")) {
+            // define ship id1 with munition[s] (idn+)
+            this.id = this.words[2];
+            // TODO: NICK - loop words 5+ into this.idN
+            System.out.println("Use CommandActorDefineShip");
+            // TODO: Use CommandActorDefineShip
+        }
+        else if (this.words[1].equals("actor")) {
+            // create actor id1 from id2 at coordinates with course course speed speed
+            this.id = this.words[2];
+            this.id2 = this.words[4];
+            this.coordinates = Integer.parseInt(this.words[6]);
+            this.course = Integer.parseInt(this.words[9]);
+            this.course = Integer.parseInt(this.words[11]);
+            System.out.println("Use CommandActorCreateActor");
+            // TODO: Use CommandActorCreateActor
+        }
+        else if (this.words[0].equals("set") &&
+                (this.words[2].equals("course") || this.words[2].equals("speed")
+                        || this.words[2].equals("depth") || this.words[2].equals("altitude"))) {
+            switch(this.words[2]) {
+                case "course":
+                    // set id course course
+                    this.id = this.words[1];
+                    this.course =  Integer.parseInt(this.words[3]);
+                    System.out.println("Use CommandActorSetCourse");
+                    // TODO: Use CommandActorSetCourse
+                    break;
+                case "speed":
+                    // set id speed speed
+                    this.id = this.words[1];
+                    this.speed =  Integer.parseInt(this.words[3]);
+                    System.out.println("Use CommandActorSetSpeed");
+                    // TODO: Use CommandActorSetSpeed
+                    break;
+                case "depth" :
+                case "altitude" :
+                    // set id altitude|depth altitude
+                    this.id = this.words[1];
+                    this.altitude =  Integer.parseInt(this.words[3]);
+                    System.out.println("Use CommandActorSetAltitudeDepth");
+                    // TODO: Use CommandActorSetAltitudeDepth
+                    break;
+            }
+        }
+    }
+
+    public void munitions() {
+        // III. MUNITIONS
+        if (this.words[1].equals("munition") || this.words[3].equals("munition")) {
+            // define
+            switch(this.words[2]) {
+                case "bomb":
+                    // define munition bomb id
+                    this.id = this.words[3];
+                    System.out.println("Use CommandMunitionDefineBomb");
+                    // TODO: Use CommandMunitionDefineBomb
+                    break;
+                case "shell":
+                    // define munition shell id
+                    this.id = this.words[3];
+                    System.out.println("Use CommandMunitionDefineShell");
+                    // TODO: Use CommandMunitionDefineShell
+                    break;
+                case "depth_charge":
+                    // define munition depth_charge id1 with fuze id2
+                    this.id = this.words[3];
+                    this.id2 = this.words[6];
+                    System.out.println("Use CommandMunitionDefineDepthCharge");
+                    // TODO: Use CommandMunitionDefineDepthCharge
+                    break;
+                case "torpedo":
+                    // define munition torpedo id1 with sensor id2 fuze id3 arming time time
+                    this.id = this.words[3];
+                    this.id2 = this.words[7];
+                    this.id3 = this.words[9];
+                    this.time = Double.parseDouble(this.words[7]);
+                    System.out.println("Use CommandMunitionDefineTorpedo");
+                    // TODO: Use CommandMunitionDefineTorpedo
+                    break;
+                case "missile":
+                    // define munition missile id1 with sensor id2 fuze id3 arming distance distance
+                    this.id = this.words[3];
+                    this.id2 = this.words[7];
+                    this.id3 = this.words[9];
+                    this.distance = Double.parseDouble(this.words[7]);
+                    System.out.println("Use CommandMunitionDefineMissile");
+                    // TODO: Use CommandMunitionDefineMissile
+                    break;
+            }
+            // set
+            if (this.words[2].equals("load")) {
+                // set id1 load munition id2
+                this.id = this.words[1];
+                this.id2 = this.words[4];
+                System.out.println("Use CommandActorLoadMunition");
+                // TODO: Use CommandActorLoadMunition
+            }
+            else if (this.words[2].equals("deploy") && this.words.length == 5) {
+                // set id1 deploy munition id2
+                this.id = this.words[1];
+                this.id2 = this.words[4];
+                System.out.println("Use CommandActorDeployMunition");
+                // TODO: Use CommandActorDeployMunition
+            }
+            else if (this.words[2].equals("deploy") && this.words.length > 5) {
+                // set id1 deploy munition id2 at azimuth azimuth elevation elevation
+                this.id = this.words[1];
+                this.id2 = this.words[4];
+                this.azimuth = Double.parseDouble(this.words[7]);
+                this.elevation = Double.parseDouble(this.words[9]);
+                System.out.println("Use CommandActorDeployMunitionShell");
+                // TODO: Use CommandActorDeployMunitionShell
+            }
+        }
+    }
+
+    public void sensorsFuzes() {
+        // IV. SENSORS/FUZES
+        if (this.words[1].equals("sensor")) {
+            switch(this.words[2]) {
+                case "radar":
+                    // define sensor radar id with field of view fov power power sensitivity sensitivity
+                    this.id = this.words[3];
+                    this.fov = Double.parseDouble(this.words[8]);
+                    this.power = Double.parseDouble(this.words[10]);
+                    this.sensitivity = Double.parseDouble(this.words[12]);
+                    System.out.println("Use CommandSensorDefineRadar");
+                    // TODO: Use CommandSensorDefineRadar
+                    break;
+                case "thermal":
+                    // define sensor thermal id with field of view fov sensitivity sensitivity
+                    this.id = this.words[3];
+                    this.fov = Double.parseDouble(this.words[8]);
+                    this.sensitivity = Double.parseDouble(this.words[10]);
+                    System.out.println("Use CommandSensorDefineRadar");
+                    // TODO: Use CommandSensorDefineThermal
+                    break;
+                case "acoustic":
+                    // define sensor acoustic id with sensitivity sensitivity
+                    this.id = this.words[3];
+                    this.sensitivity = Double.parseDouble(this.words[6]);
+                    System.out.println("Use CommandSensorDefineAcoustic");
+                    // TODO: Use CommandSensorDefineAcoustic
+                    break;
+                case "depth":
+                    // define sensor depth id with trigger depth altitude
+                    this.id = this.words[3];
+                    this.altitude = Integer.parseInt(this.words[7]);
+                    System.out.println("Use CommandSensorDefineDepth");
+                    // TODO: Use CommandSensorDefineDepth
+                    break;
+                case "distance":
+                    // define sensor distance id with trigger distance distance
+                    this.id = this.words[3];
+                    this.distance = Integer.parseInt(this.words[7]);
+                    System.out.println("Use CommandSensorDefineDistance");
+                    // TODO: Use CommandSensorDefineDistance
+                    break;
+                case "time":
+                    // define sensor time id with trigger time time
+                    this.id = this.words[3];
+                    this.time = Integer.parseInt(this.words[7]);
+                    System.out.println("Use CommandSensorDefineTime");
+                    // TODO: Use CommandSensorDefineTime
+                    break;
+
+            }
+            if (this.words[3].equals("active")) {
+                // define sensor sonar active id with power power sensitivity sensitivity
+                this.id = this.words[4];
+                this.power = Double.parseDouble(this.words[7]);
+                this.sensitivity = Double.parseDouble(this.words[9]);
+                System.out.println("Use CommandSensorDefineSonarActive");
+                // TODO: Use CommandSensorDefineSonarActive
+            }
+            else if (this.words[3].equals("passive")) {
+                // define sensor sonar passive id with sensitivity sensitivity
+                this.id = this.words[4];
+                this.sensitivity = Double.parseDouble(this.words[7]);
+                System.out.println("Use CommandSensorDefineSonarPassive");
+                // TODO: Use CommandSensorDefineSonarPassive
+            }
+        }
+    }
+
+    public void miscellaneous() {
+        // VII. MISCELLANEOUS
+        if (this.words[0].charAt(0) == '@'){
+            switch(this.words[0]) {
+                case "@load":
+                    // @load filename
+                    this.fileName = this.words[1];
+                    System.out.println("Use CommandMiscLoad");
+                    // TODO: Use CommandMiscLoad
+                    break;
+                case "@pause":
+                    // @pause
+                    System.out.println("Use CommandMiscPause");
+                    // TODO: Use CommandMiscPause
+                    break;
+                case "@resume":
+                    // @resume
+                    System.out.println("Use CommandMiscResume");
+                    // TODO: Use CommandMiscResume
+                    break;
+                case "@set":
+                    // @set update time
+                    this.time = Double.parseDouble(this.words[2]);
+                    System.out.println("Use CommandMiscSetUpdate");
+                    // TODO: Use CommandMiscSetUpdate
+                    break;
+                case "@wait":
+                    // @wait time
+                    this.time = Double.parseDouble(this.words[2]);
+                    System.out.println("Use CommandMiscWait");
+                    // TODO: Use CommandMiscWait
+                    break;
+                case "@force":
+                    // @force id state to coordinates with course course speed speed
+                    this.id = this.words[1];
+                    this.course = Integer.parseInt(this.words[4]);
+                    this.speed = Integer.parseInt(this.words[9]);
+                    System.out.println("Use CommandActorSetState");
+                    // TODO: Use CommandActorSetState.
+                    break;
+                case "@exit":
+                    // @exit
+                    System.out.println("Use CommandMiscExit");
+                    // TODO: Use CommandMiscExit
+                    break;
+            }
+        }
+    }
+
+    public void loadKeyWords() {
+        keyWords.add("@load");
+        keyWords.add("@pause");
+        keyWords.add("@resume");
+        keyWords.add("@set");
+        keyWords.add("@wait");
+        keyWords.add("@force");
+        keyWords.add("@exit");
+
+        keyWords.add("acoustic");
+        keyWords.add("active");
+        keyWords.add("actor");
+        keyWords.add("airplane");
+        keyWords.add("altitude");
+        keyWords.add("arming");
+        keyWords.add("at");
+        keyWords.add("azimuth");
+        keyWords.add("bomb");
+        keyWords.add("command");
+        keyWords.add("coordinates");
+        keyWords.add("course");
+        keyWords.add("create");
+        keyWords.add("define");
+        keyWords.add("delete");
+        keyWords.add("deploy");
+        keyWords.add("depth");
+        keyWords.add("depth_charge");
+        keyWords.add("distance");
+        keyWords.add("elevation");
+        keyWords.add("execute");
+        keyWords.add("field");
+        keyWords.add("fov");
+        keyWords.add("from");
+        keyWords.add("front");
+        keyWords.add("fuze");
+        keyWords.add("id");
+        keyWords.add("latitude");
+        keyWords.add("load");
+        keyWords.add("lock");
+        keyWords.add("longitude");
+        keyWords.add("maneuver");
+        keyWords.add("missile");
+        keyWords.add("munition");
+        keyWords.add("of");
+        keyWords.add("passive");
+        keyWords.add("power");
+        keyWords.add("radar");
+        keyWords.add("sensitivity");
+        keyWords.add("sensor");
+        keyWords.add("set");
+        keyWords.add("side");
+        keyWords.add("shell");
+        keyWords.add("ship");
+        keyWords.add("sonar");
+        keyWords.add("speed");
+        keyWords.add("state");
+        keyWords.add("submarine");
+        keyWords.add("thermal");
+        keyWords.add("time");
+        keyWords.add("to");
+        keyWords.add("top");
+        keyWords.add("torpedo");
+        keyWords.add("trigger");
+        keyWords.add("undefine");
+        keyWords.add("unlock");
+        keyWords.add("update");
+        keyWords.add("view");
+        keyWords.add("window");
+        keyWords.add("with");
+    }
+}
---
+++ cs350s21project.cli/ParserTester.java
@@ -0,0 +1,20 @@
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.HashSet;
+
+
+public class ParserTester {
+
+    public static void main(String[] args) {
+
+        // !!!THIS LOOP DOESNT END FOR TESTING, YOU WILL NEED TO MANUALLY END IT!!!!
+
+        int i = 1;
+        int j = 2;
+        while(i < j) {
+            Parser parser = new Parser();
+            parser.runParser();
+        }
+    }
+
+}
