8f6e9d82804fa0931a1bbb5df197176bbe037ee6
Luis Garcia <lgarcia29@eagles.ewu.edu>
2021-06-01 17:49:39 -0700

    adding to actors

--- cs350s21project.cli/Actors.java
+++ cs350s21project.cli/Actors.java
@@ -6,7 +6,6 @@ import cs350s21project.controller.command.actor.CommandActorDefineShip;
 import cs350s21project.controller.command.actor.CommandActorSetAltitudeDepth;
 import cs350s21project.controller.command.actor.CommandActorSetCourse;
 import cs350s21project.datatype.*;
-
 import java.util.List;
 
 public class Actors {
@@ -20,20 +19,24 @@ public class Actors {
     public void createActor(CommandManagers managers, String text, AgentID idActor, AgentID idFamily, CoordinateWorld3D position, Course course, Groundspeed speed)
     {
         CommandActorCreateActor createActor = new CommandActorCreateActor(managers, text, idActor,idFamily, position, course, speed);
+        createActor.execute();
     }
 
     public void setCourseId(CommandManagers managers, String text, AgentID idActor, Course course)
     {
         CommandActorSetCourse setCourse = new CommandActorSetCourse(managers, text, idActor, course);
+        setCourse.execute();
     }
 
     public void setSpeedId(CommandManagers managers, String text, AgentID idActor, Course course)
     {
        CommandActorSetCourse setCourse = new CommandActorSetCourse(managers, text, idActor, course);
+       setCourse.execute();
     }
 
     public void setAltitude(CommandManagers managers, String text, AgentID idActor, Altitude altitude)
     {
         CommandActorSetAltitudeDepth setAltitudeDepth = new CommandActorSetAltitudeDepth(managers, text, idActor, altitude);
+        setAltitudeDepth.execute();
     }
 }
